6248f1aad80ad513f89ef8f60146838f2a4e4de5
ramusus <ramusus@gmail.com>
2014-11-03 22:47:23 +0300

    bump version

--- kinopoisk/__init__.py
+++ kinopoisk/__init__.py
@@ -1,7 +1,7 @@
 from .movie import Movie, Trailer
 from .person import Person
 
-VERSION = (0, 5, 4)
+VERSION = (0, 5, 5)
 __version__ = '.'.join(map(str, VERSION))
 
 __all__ = ['Movie', 'Person', 'Trailer']
