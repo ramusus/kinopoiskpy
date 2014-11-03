a03c02cb42bc11623f8a102140622c90e6710ae0
ramusus <ramusus@gmail.com>
2014-11-03 22:46:34 +0300

    try to fix install error

--- kinopoisk/__init__.py
+++ kinopoisk/__init__.py
@@ -1,7 +1,7 @@
-from movie import Movie, Trailer
-from person import Person
+from .movie import Movie, Trailer
+from .person import Person
 
-VERSION = (0, 5, 3)
+VERSION = (0, 5, 4)
 __version__ = '.'.join(map(str, VERSION))
 
 __all__ = ['Movie', 'Person', 'Trailer']
