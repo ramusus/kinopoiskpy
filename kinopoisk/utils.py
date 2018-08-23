bd2d4f7db44540ae1105ca7948fde4b0b837cb77
ramusus <ramusus@gmail.com>
2018-08-22 21:23:42 -0400

    fix lint

--- kinopoisk/utils.py
+++ kinopoisk/utils.py
@@ -321,9 +321,7 @@ def restore_windows_1252_characters(s):
     """Replace C1 control characters in the Unicode string s by the
     characters at the corresponding code points in Windows-1252,
     where possible.
-
     """
-    import re
     def to_windows_1252(match):
         try:
             return bytes([ord(match.group(0))]).decode('windows-1252')
